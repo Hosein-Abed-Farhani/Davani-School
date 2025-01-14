f528b147941eb3e33f0e336c73438c57f83698a1
Hosein <hosinfarhani98@gmail.com>
2025-01-14 14:07:36 +0330

    edit info page - add information

--- Info-Page/script.js
+++ Info-Page/script.js
@@ -25,11 +25,11 @@ function scroll() {
 
   console.log(winScroll);
 
-  // box anim
+  // box - 1 - anim
 
-  // for laptop
-  if (900 < document.body.clientWidth) {
-    if (winScroll >= 480) {
+  // for L laptop
+  if (1200 < document.body.clientWidth) {
+    if (winScroll >= 400) {
       document.querySelector("#info-boxes-1").style.translate = "0px";
       document.querySelector("#info-boxes-2").style.translate = "0px";
     }
@@ -38,35 +38,100 @@ function scroll() {
       document.querySelector("#info-boxes-4").style.translate = "0px";
     }
   }
-  // for tablet
-  if (555 < document.body.clientWidth < 900) {
-    if (winScroll >= 285) {
+
+  // for laptop and tablet
+  if (900 < document.body.clientWidth) {
+    if (winScroll >= 250) {
       document.querySelector("#info-boxes-1").style.translate = "0px";
-    }
-    if (winScroll >= 585) {
       document.querySelector("#info-boxes-2").style.translate = "0px";
     }
-    if (winScroll >= 880) {
+    if (winScroll >= 550) {
+      document.querySelector("#info-boxes-3").style.translate = "0px";
+      document.querySelector("#info-boxes-4").style.translate = "0px";
+    }
+  }
+
+  // for tablet and mobile
+  if (document.body.clientWidth < 900) {
+    if (winScroll >= 180) {
+      document.querySelector("#info-boxes-1").style.translate = "0px";
+    }
+    if (winScroll >= 480) {
+      document.querySelector("#info-boxes-2").style.translate = "0px";
+    }
+    if (winScroll >= 780) {
       document.querySelector("#info-boxes-3").style.translate = "0px";
     }
-    if (winScroll >= 1180) {
+    if (winScroll >= 980) {
       document.querySelector("#info-boxes-4").style.translate = "0px";
     }
   }
 
   // for mobile
   if (document.body.clientWidth < 555) {
-    if (winScroll >= 50) {
+    if (winScroll >= 400) {
       document.querySelector("#info-boxes-1").style.translate = "0px";
     }
-    if (winScroll >= 350) {
+    if (winScroll >= 550) {
       document.querySelector("#info-boxes-2").style.translate = "0px";
     }
-    if (winScroll >= 650) {
+    if (winScroll >= 950) {
       document.querySelector("#info-boxes-3").style.translate = "0px";
     }
-    if (winScroll >= 950) {
+    if (winScroll >= 1300) {
       document.querySelector("#info-boxes-4").style.translate = "0px";
+    }
+  }
+
+  // box - 2 - anim
+
+  // for L laptop
+  if (1200 < document.body.clientWidth) {
+    if (winScroll >= 950) {
+      document.querySelector("#dav-box-1").style.opacity = "1";
+    }
+    if (winScroll >= 1100) {
+      document.querySelector("#dav-box-2").style.opacity = "1";
+    }
+  }
+
+  //for laptop and tablet
+  if (1000 < document.body.clientWidth) {
+    if (winScroll >= 1150) {
+      document.querySelector("#dav-box-1").style.opacity = "1";
+    }
+    if (winScroll >= 1300) {
+      document.querySelector("#dav-box-2").style.opacity = "1";
+    }
+  }
+
+  //for laptop and tablet
+  if (900 < document.body.clientWidth) {
+    if (winScroll >= 1000) {
+      document.querySelector("#dav-box-1").style.opacity = "1";
+    }
+    if (winScroll >= 1150) {
+      document.querySelector("#dav-box-2").style.opacity = "1";
+    }
+  }
+
+  // for tablet and mobile
+  if (555 < document.body.clientWidth) {
+    if (winScroll >= 1500) {
+      document.querySelector("#dav-box-1").style.opacity = "1";
+    }
+    if (winScroll >= 1700) {
+      document.querySelector("#dav-box-2").style.opacity = "1";
+    }
+  }
+
+  // for mobile
+  if (document.body.clientWidth < 555) {
+    if (winScroll >= 1800) {
+      document.querySelector("#dav-box-1").style.opacity = "1";
+    }
+    if (winScroll >= 2000) {
+      document.querySelector("#dav-box-2").style.opacity = "1";
     }
   }
 }
